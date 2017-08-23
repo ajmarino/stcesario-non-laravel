1ef2d6303a86207c763efd756f0ac66f7e4d8600
AJ Marino <aj@ajmarino.com>
2017-08-22 22:00:33 -0400

    fix: Adds test images

--- resources/views/components/jumbotron.php
+++ resources/views/components/jumbotron.php
@@ -1,4 +1,4 @@
-<section class="bg-dark-gray white bg-fixed cover" style="background-image: url('https://source.unsplash.com/random/1730x1135')">
+<section class="bg-dark-gray white bg-fixed cover" style="background-image: url('/images/walking-up-the-street.jpg'); filter: grayscale();">
     <div class="min-h-45 vh-100 w-100 flex items-center bg-black-60">
         <div class="mw8 center tc pt5 pt3-l">
             <h1 class="f1 f-subheadline-l ttu normal">Feast of St. Cesario</h1>
