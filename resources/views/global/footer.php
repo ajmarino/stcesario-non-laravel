915be0cd1ca06fbfc8c18444749ab3486b7aa9db
AJ Marino <aj@ajmarino.com>
2017-08-22 22:01:34 -0400

    fix: Clean up header, footer, and logo styles

--- resources/views/global/footer.php
+++ resources/views/global/footer.php
@@ -1,13 +1,4 @@
-<footer class="c-global-footer">
-    <div class="l-container">
-        <p class="social__text">Connect with us on <a href="#" class="c-global-footer__link">Facebook</a> </p>
-
-        <p class="c-global-footer__text">
-            Copyright &copy; <?php echo Date("Y"); ?> St. Cesario Society. All Rights Reserved
-
-            <a href="humans.txt" class="c-global-footer__link" title="Made by Humans">
-                <img src="/images/humanstxt-reg.png" alt="Made by humans">
-            </a>
-        </p>
-    </div>
+<footer class="w-100 bg-near-white tc pv5 silver">
+    <p class="f5 mb3">Connect with us on <a href="#" class="link mid-gray hover-silver no-underline">Facebook</a> </p>
+    <p class="f6 mb0">Copyright &copy; <?php echo Date("Y"); ?> St. Cesario Society. All Rights Reserved</p>
 </footer>
--- resources/views/global/header.php
+++ resources/views/global/header.php
@@ -1,12 +1,12 @@
-<header class="fixed w-100 bg-white" ref="header">
-	<div class="mw8 center flex flex-column flex-row-l items-center justify-between pa3">
-		<a href="#top" class="logo ff-oswald f2 lh-title mb3 mb1-l tc no-underline ttu js-slide" @click.prevent="slideTo($event)">St. Cesario Society</a>
+<header class="fixed z-5 w-100 bb b--moon-gray bg-white" ref="header">
+    <div class="mw8 center flex flex-column flex-row-l items-center justify-between pa3">
+        <a href="#top" class="logo shadow-special ff-oswald f2 lh-title mb3 mb1-l tc no-underline ttu js-slide" @click.prevent="slideTo($event)">St. Cesario Society</a>
 
-		<nav class="flex flex-wrap tc">
-			<a href="#about-us" class="w-50 w-auto-l link no-underline gray hover-mid-gray mh3-l" @click.prevent="slideTo($event)">About Us</a>
-			<a href="#our-history" class="w-50 w-auto-l link no-underline gray hover-mid-gray mh3-l" @click.prevent="slideTo($event)">Our History</a>
-			<a href="#media" class="w-50 w-auto-l link no-underline gray hover-mid-gray mh3-l" @click.prevent="slideTo($event)">Media</a>
-			<a href="#contact-us" class="w-50 w-auto-l link no-underline gray hover-mid-gray mh3-l" @click.prevent="slideTo($event)">Contact Us</a>
-		</nav>
-	</div>
+        <nav class="flex flex-wrap tc">
+            <a href="#about-us" class="w-50 w-auto-l link no-underline gray hover-dark-gray mh3-l" @click.prevent="slideTo($event)">About Us</a>
+            <a href="#our-history" class="w-50 w-auto-l link no-underline gray hover-dark-gray mh3-l" @click.prevent="slideTo($event)">Our History</a>
+            <a href="#media" class="w-50 w-auto-l link no-underline gray hover-dark-gray mh3-l" @click.prevent="slideTo($event)">Media</a>
+            <a href="#contact-us" class="w-50 w-auto-l link no-underline gray hover-dark-gray mh3-l" @click.prevent="slideTo($event)">Contact Us</a>
+        </nav>
+    </div>
 </header>
