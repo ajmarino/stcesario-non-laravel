07cf343867884da3dda11f304f1c0e3ff87c3f52
AJ Marino <aj@ajmarino.com>
2017-09-19 09:51:05 -0400

    fix: Updates to divider sections

--- resources/views/components/parallax/block-2.php
+++ resources/views/components/parallax/block-2.php
@@ -1,8 +1,8 @@
-<section class="c-parallax c-parallax--two">
-	<div class="c-parallax__bg"></div>
-	
-	<div class="c-parallax__overlay">
-		<h2 class="c-parallax__title">Something goes here</h2>
-		<p class="c-parallax__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, fuga impedit repellat inventore quidem consectetur debitis repellendus autem quisquam laborum quasi enim voluptas, quos dolorem.</p>
-	</div>
+<section class="vh-100 max-h-40 cover" style="background-image: url('/images/cesa-church.jpg');">
+    <div class="bg-black-70 h-100 w-100 flex items-center justify-center">
+        <div class="measure-wide tc">
+            <h2 class="dib ff-oswald f1 ttu bb bt b--white ls-05 darkest-red ph2">Something goes here</h2>
+            <p class="f4 white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, fuga impedit repellat inventore quidem consectetur debitis repellendus autem quisquam laborum quasi enim voluptas, quos dolorem.</p>
+        </div>
+    </div>
 </section>
